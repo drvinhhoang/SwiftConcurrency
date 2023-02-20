e11b442a2f82dea5adc326760f300a39c643b3d7
vinhhoang <dr.vinhhoang@gmail.com>
2023-02-20 21:32:46 +0700

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by VinhHoang on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
